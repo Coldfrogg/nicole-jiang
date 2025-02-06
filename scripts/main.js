2024552507a981b4c60c7db12bd546b4358dd8a9
Nicole Jiang <nicolejiang@Nicoles-MacBook-Pro-3.local>
2025-02-06 13:23:29 +1100

    added typing text

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Welcome to my personal website!');
   });
-  
+
+const text = "welcome to my personal website!";
+const typedTextElement = document.getElementById('typed-text');
+let index = 0;
+
+function typeText() {
+  if (index < text.length) {
+    typedTextElement.innerHTML += text.charAt(index);
+    index++;
+    setTimeout(typeText, 100); // Adjust speed of typing here
+  }
+}
+
+window.onload = typeText;
